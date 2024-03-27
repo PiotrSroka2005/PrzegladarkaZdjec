e3cbbee7f0769232343bb1d03386ae570b5f5c44
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-03-27 10:23:35 +0100

    Add OpenDirectory function for select of images from folder

--- PrzegladarkaZdjec/MainWindow.xaml.cs
+++ PrzegladarkaZdjec/MainWindow.xaml.cs
@@ -27,6 +27,33 @@ namespace PrzegladarkaZdjec
             InitializeComponent();
         }
 
-        
+        private void OpenDirectory(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Wybierz folder";
+            dialog.UseDescriptionForTitle = true;
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                paths.Clear();
+                string[] extensions = new string[] { ".png", ".gif", ".jpg", ".jpeg", ".bmp" };
+                foreach (string extension in extensions)
+                    paths.AddRange(Directory.GetFiles(dialog.SelectedPath, "*" + extension));
+
+
+                if (paths.Count > 0)
+                {
+                    fitBtn.IsEnabled = false;
+                    originalBtn.IsEnabled = true;
+                    rotation = 0;
+                    DisplayImage(0);
+                }
+                else
+                {
+                    displayedImageIndex = 0;
+                    Image.Source = null;
+                }
+            }
+        }
     }
 }
